6a190ebecd8b0501df6b5b23c40027260a6363fd
rajatgms <23decrajat@gmail.com>
2022-01-24 12:36:53 +0530

    Rajat - Search location using Google Map

--- ReadMe.md
+++ ReadMe.md
@@ -7,3 +7,10 @@
   - `git checkout react-js`
 - For Node JS
   - `git checkout node-js`
+
+
+####Vanilla JS Learning
+  - axios in-built provide type definition
+  - google-maps needs type definition file
+  - Create [GoogleAPIKey](https://developers.google.com/maps/documentation/javascript/get-api-key)
+  - If you don't have GoogleAPIKey
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,41 @@
-// Code goes here!
+import axios from "axios";
+
+const form = document.querySelector("form")!;
+const addressInput = document.getElementById("address")! as HTMLInputElement;
+const mapElement = document.getElementById("map")!;
+
+const GOOGLE_API_KEY = "GOOGLE_API_KEY";
+
+type GoogleGeocodingResponse = {
+    results: { geometry: { location: { lat: number; lng: number } } }[];
+    status: "OK" | "ZERO_RESULTS" | "REQUEST_DENIED";
+    error_message: string;
+};
+
+function searchAddressHandler(event: Event) {
+    event.preventDefault();
+    const enteredAddress = addressInput.value;
+
+    axios
+        .get<GoogleGeocodingResponse>(
+            `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}&key=${GOOGLE_API_KEY}`
+        )
+        .then(response => {
+            if (response.data.status !== "OK") {
+                throw new Error(`Could not fetch location!: - ${response.data.error_message}`);
+            }
+            const coordinates = response.data.results[0].geometry.location;
+            const map = new google.maps.Map(mapElement, {
+                center: coordinates,
+                zoom: 16
+            });
+
+            new google.maps.Marker({ position: coordinates, map: map });
+        })
+        .catch(err => {
+            alert(err.message);
+            console.log(err);
+        });
+}
+
+form.addEventListener("submit", searchAddressHandler);
